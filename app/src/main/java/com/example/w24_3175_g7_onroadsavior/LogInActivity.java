ab705436ec6f2794b07dde545d9871efd385768b
SaubhagyaM1996 <riligalak@student.douglascollege.ca>
2024-03-14 13:50:14 -0700

    regForm and profile layouts

--- app/src/main/java/com/example/w24_3175_g7_onroadsavior/LogInActivity.java
+++ app/src/main/java/com/example/w24_3175_g7_onroadsavior/LogInActivity.java
@@ -2,7 +2,11 @@ package com.example.w24_3175_g7_onroadsavior;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button btnSignIn = findViewById(R.id.btnSignIn);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogInActivity.this, FragmentHandler.class));
+            }
+        });
     }
 }
