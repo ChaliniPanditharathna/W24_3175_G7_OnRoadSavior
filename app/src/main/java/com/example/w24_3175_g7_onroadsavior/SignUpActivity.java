584cbecd4b45de01c2140bb1199c38ad2233333d
SaubhagyaM1996 <riligalak@student.douglascollege.ca>
2024-03-15 22:33:34 -0700

    email password authentication

--- app/src/main/java/com/example/w24_3175_g7_onroadsavior/SignUpActivity.java
+++ app/src/main/java/com/example/w24_3175_g7_onroadsavior/SignUpActivity.java
@@ -5,34 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
 
     FirebaseDatabase rootNode;
     DatabaseReference reference;
-    int serviceRequesterCount = 0;
-    int serviceProviderCount = 0;
-
-
     String fullName, username, email, contactNumber, password, userType, serviceType;
+
+    private static final String TAG = "EmailPassword";
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         //widgets
         TextInputLayout regFullName = findViewById(R.id.name);
@@ -80,24 +88,20 @@ public class SignUpActivity extends AppCompatActivity {
                 contactNumber = regContactNumber.getEditText().getText().toString();
                 password = regPassword.getEditText().getText().toString();
 
-
                 int userTypeInd = userTypeSpinner.getSelectedItemPosition();
 
                 if (userTypeInd == 0){
                     userType = "Service Requester";
                     serviceType = "";
-
-                    generateRecode(fullName, username, email, contactNumber, password, userTypeInd, userType, serviceType);
-;
                 } else if (userTypeInd == 1){
                     userType = "Service Provider";
-
                     int serviceTypeInd = serviceTypeSpinner.getSelectedItemPosition();
                     serviceType = getServiceType(serviceTypeInd);
-
-                    generateRecode(fullName, username, email, contactNumber, password, userTypeInd, userType, serviceType);
                 }
+
+                createAccount(email, password, fullName, username, contactNumber, userType, serviceType);
             }
+
         });
 
 
@@ -111,12 +115,12 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
-    private void generateRecode(String fullname, String userName, String email, String contactNumber, String password, int userTypeInd, String userType, String serviceType){
+    private void generateUserRecord(String fullname, String userName, String email, String contactNumber, String password, String userType, String serviceType){
 
         rootNode = FirebaseDatabase.getInstance();
         reference = rootNode.getReference("Users");
 
-        DatabaseReference userNameRef = reference.child(userName);
+        DatabaseReference userNameRef = reference.child(email);
         ValueEventListener eventListener = new ValueEventListener() {
 
             @Override
@@ -128,7 +132,7 @@ public class SignUpActivity extends AppCompatActivity {
                     UserHelperClass user = new UserHelperClass(fullName, username, email, contactNumber, password, userType, serviceType);
 
                     // Save the user data in the appropriate table based on userType
-                    reference.child(username).setValue(user);
+                    reference.child(email).setValue(user);
 
                     Toast.makeText(SignUpActivity.this, "User registered successfully!", Toast.LENGTH_SHORT).show();
 
@@ -171,6 +175,58 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
         return serviceType;
+    }
+
+    //When initializing Activity, check to see if the user is currently signed in
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            loadMainActivity(currentUser);
+        }
+    }
+    // [END on_start_check_user]
+
+
+    //Create a new account by passing the new user's email address and password to createUserWithEmailAndPassword
+    private void createAccount(String email, String password, String fullName, String username, String contactNumber, String userType, String serviceType) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            generateUserRecord(fullName, username, email, contactNumber, password, userType, serviceType);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void loadMainActivity(FirebaseUser currentUser) {
+        String email = currentUser.getEmail();
+        Bundle bundle = new Bundle();
+        bundle.putString("EMAIL",email);
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 
 
