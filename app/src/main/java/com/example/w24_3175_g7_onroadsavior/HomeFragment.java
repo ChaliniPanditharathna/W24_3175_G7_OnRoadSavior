2bd4f0a3763247b9698586d90c5da877976446e0
SaubhagyaM1996 <riligalak@student.douglascollege.ca>
2024-03-17 00:07:08 -0700

    removed unused code in homeFragment

--- app/src/main/java/com/example/w24_3175_g7_onroadsavior/HomeFragment.java
+++ app/src/main/java/com/example/w24_3175_g7_onroadsavior/HomeFragment.java
@@ -1,43 +1,20 @@
 package com.example.w24_3175_g7_onroadsavior;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.auth.FirebaseAuth;
-
 public class HomeFragment extends Fragment {
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button btnTestLogOutTemp = v.findViewById(R.id.btnTestLogOutTemp);
-
-        btnTestLogOutTemp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                signOut();
-                Intent intent = new Intent(getActivity(), LogInActivity.class);
-                startActivity(intent);
-            }
-        });
-
         return v;
-    }
-
-    public void signOut() {
-        // [START auth_sign_out]
-        FirebaseAuth.getInstance().signOut();
-        // [END auth_sign_out]
     }
 }
