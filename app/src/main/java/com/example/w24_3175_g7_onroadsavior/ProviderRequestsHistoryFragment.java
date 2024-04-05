869220ec6e31095debb995319752a670be1a4236
SubashiniPiumali <subashinipiumali1994@gmail.com>
2024-04-05 12:15:03 -0700

    request history view and completion the request

--- app/src/main/java/com/example/w24_3175_g7_onroadsavior/ProviderRequestsHistoryFragment.java
+++ app/src/main/java/com/example/w24_3175_g7_onroadsavior/ProviderRequestsHistoryFragment.java
@@ -1,15 +1,7 @@
 package com.example.w24_3175_g7_onroadsavior;
 
-import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,38 +10,40 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.example.w24_3175_g7_onroadsavior.Database.DBHelper;
-import com.example.w24_3175_g7_onroadsavior.Interface.ProviderRequestInterface;
+import com.example.w24_3175_g7_onroadsavior.Interface.ProviderRequestHistoryInterface;
 import com.example.w24_3175_g7_onroadsavior.Model.RequestDetails;
+import com.example.w24_3175_g7_onroadsavior.adapter.ProviderRequestHistoryAdapter;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ServiceProviderHistoryFragment#newInstance} factory method to
+ * Use the {@link ProviderRequestsHistoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ServiceProviderRequestFragment extends Fragment implements ProviderRequestInterface {
+public class ProviderRequestsHistoryFragment extends Fragment implements ProviderRequestHistoryInterface {
     RecyclerView recyclerView;
     ArrayList<RequestDetails> requestDetails;
     DBHelper dbHelper;
-    ProviderRequetsAdapter requetsAdapter;
-
+    ProviderRequestHistoryAdapter requetsAdapter;
     FirebaseUser currentUser;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_service_provider_request, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_provider_requests_history, container, false);
         dbHelper = new DBHelper(view.getContext());
         requestDetails = new ArrayList<>();
-        recyclerView = view.findViewById(R.id.requestrecylerview);
-        requetsAdapter = new ProviderRequetsAdapter(view.getContext(), requestDetails, this);
+        recyclerView = view.findViewById(R.id.requestrecylerViewProviderRequestHistroy);
+        requetsAdapter = new ProviderRequestHistoryAdapter(view.getContext(), requestDetails, this);
         recyclerView.setAdapter(requetsAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
         displayData();
@@ -78,9 +72,9 @@ public class ServiceProviderRequestFragment extends Fragment implements Provider
 
         if (args != null) {
             currentUser = args.getParcelable("CURRENT_USER");
-            Cursor cursor = dbHelper.getRequestData(currentUser.getUid());
+            Cursor cursor = dbHelper.getRequestHistoryData(currentUser.getUid());
             if (cursor.getCount() == 0) {
-                Toast.makeText(ServiceProviderRequestFragment.this.getContext(), "No entry exists", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this.getContext(), "No entry exists", Toast.LENGTH_SHORT).show();
                 return;
             } else {
                 while (cursor.moveToNext()) {
@@ -98,7 +92,6 @@ public class ServiceProviderRequestFragment extends Fragment implements Provider
                             cursor.getString(10),
                             cursor.getString(11),
                             cursor.getString(12)
-
                     );
 
                     requestDetails.add(req);
@@ -107,12 +100,14 @@ public class ServiceProviderRequestFragment extends Fragment implements Provider
             }
         }
 
+
     }
 
     @Override
     public void OnItemClick(int position) {
         Bundle result = new Bundle();
         RequestDetails req = requestDetails.get(position);
+
         result.putString("USERNAME", req.getUserName());
         result.putString("USERID", req.getUserId());
         result.putString("BREAKDOWNTYPE", req.getBreakDownType());
@@ -121,59 +116,20 @@ public class ServiceProviderRequestFragment extends Fragment implements Provider
         result.putString("LOCATION", req.getLocation());
         result.putString("DESCRIPTION", req.getDescription());
         result.putString("IMAGEURL", req.getImageUrl());
-
-        if (req.getStatus().equals("Accept")) {
-            if (req.getStatus().equals("Done")) {
-                result.putString("MESSAGE", "Successfully Done");
-            }
-            if (req.getStatus().equals("Reject")) {
-                result.putString("MESSAGE", "You rejected this request");
-            }
-            if (req.getStatus().equals("Accept")) {
-                result.putString("MESSAGE", "Ongoing request");
-            }
-
-            Fragment providerRequestHistoryFragment = new ProviderRequestHistoryFragment();
-            providerRequestHistoryFragment.setArguments(result);
-            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.frame_layout, providerRequestHistoryFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
+        if (req.getStatus().equals("Done")) {
+            result.putString("MESSAGE", "Successfully Done");
         }
-        if (req.getStatus().equals("Pending")) {
-            result.putString("BREAKDOWNREQUESTID", req.getBreakDownRequestId());
-
-            Fragment userRequestAcceptFragment = new UserRequestAcceptFragment();
-            userRequestAcceptFragment.setArguments(result);
-            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.frame_layout, userRequestAcceptFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
+        if (req.getStatus().equals("Reject")) {
+            result.putString("MESSAGE", "You rejected this request");
         }
 
-    }
-
-    public void onCompleteClick(int position) {
-        // Update the database value based on the position
-        // For example:
-        RequestDetails req = requestDetails.get(position);
-        boolean isUpdated = dbHelper.updateStatus(req.getBreakDownRequestId());
-        if (isUpdated) {
-            Toast.makeText(getContext(), "Completed successfully", Toast.LENGTH_SHORT).show();
-            requestDetails.remove(position);
-            // Fetch updated data from the database
-            // Notify the adapter about the changes
-            requetsAdapter.notifyItemRemoved(position);
-            requestDetails.clear();
-            displayData();
-        } else {
-            Toast.makeText(getContext(), "Failed to Completed", Toast.LENGTH_SHORT).show();
-        }
-
-        // Notify the adapter that the data has changed
-
+        Fragment providerRequestHistoryFragment = new ProviderRequestHistoryFragment();
+        providerRequestHistoryFragment.setArguments(result);
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, providerRequestHistoryFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
--- app/src/main/java/com/example/w24_3175_g7_onroadsavior/adapter/ProviderRequestHistoryAdapter.java
+++ app/src/main/java/com/example/w24_3175_g7_onroadsavior/adapter/ProviderRequestHistoryAdapter.java
@@ -1,44 +1,43 @@
-package com.example.w24_3175_g7_onroadsavior;
+package com.example.w24_3175_g7_onroadsavior.adapter;
 
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.w24_3175_g7_onroadsavior.Interface.ProviderRequestInterface;
+import com.example.w24_3175_g7_onroadsavior.Interface.ProviderRequestHistoryInterface;
 import com.example.w24_3175_g7_onroadsavior.Model.RequestDetails;
+import com.example.w24_3175_g7_onroadsavior.R;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class ProviderRequetsAdapter extends RecyclerView.Adapter<ProviderRequetsAdapter.RequestViewHolder> {
-
-    private final ProviderRequestInterface providerRequestInterface;
+public class ProviderRequestHistoryAdapter extends RecyclerView.Adapter<ProviderRequestHistoryAdapter.RequestViewHolder> {
+    private final ProviderRequestHistoryInterface providerRequestHistoryInterface;
     private Context context;
     private ArrayList<RequestDetails> requestDetails = null;
     StorageReference storageReference;
     FirebaseStorage storage;
 
-    public ProviderRequetsAdapter(Context context, ArrayList<RequestDetails> requestDetails, ProviderRequestInterface providerRequestInterface) {
+    public ProviderRequestHistoryAdapter(Context context, ArrayList<RequestDetails> requestDetails, ProviderRequestHistoryInterface providerRequestHistoryInterface) {
         this.context = context;
         this.requestDetails = requestDetails;
-        this.providerRequestInterface = providerRequestInterface;
+        this.providerRequestHistoryInterface = providerRequestHistoryInterface;
     }
 
     @NonNull
     @Override
     public RequestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.service_provider_request, parent, false);
-        return new RequestViewHolder(v, providerRequestInterface);
+        View v = LayoutInflater.from(context).inflate(R.layout.service_provider_history_request, parent, false);
+        return new RequestViewHolder(v, providerRequestHistoryInterface);
     }
 
     @Override
@@ -47,17 +46,16 @@ public class ProviderRequetsAdapter extends RecyclerView.Adapter<ProviderRequets
         holder.userName.setText(req.getUserName());
         holder.breakDownType.setText(req.getBreakDownType());
         holder.location.setText(req.getLocation());
-        if(req.getStatus().equals("Pending")){
-            holder.status.setText("New");
-            holder.btnCompleted.setEnabled(false);
+        if (req.getStatus().equals("Done")) {
+            holder.status.setText("Done");
         }
-        if(req.getStatus().equals("Accept")){
-            holder.status.setText("Ongoing");
-            holder.btnCompleted.setEnabled(true);
+        if (req.getStatus().equals("Reject")) {
+            holder.status.setText("Rejected");
         }
+
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
-        StorageReference profileImageRef = storageReference.child("profile_images/" +req.getUserId()+ ".jpg");
+        StorageReference profileImageRef = storageReference.child("profile_images/" + req.getUserId() + ".jpg");
 
         // Check if the ImageView is not null before loading the image
         if (holder.imageViewProfilePic != null) {
@@ -81,39 +79,24 @@ public class ProviderRequetsAdapter extends RecyclerView.Adapter<ProviderRequets
 
     public class RequestViewHolder extends RecyclerView.ViewHolder {
 
-        TextView  userName, breakDownType,location, status;
+        TextView userName, breakDownType, location, status;
         ImageView imageViewProfilePic;
-        Button btnCompleted;
-        public RequestViewHolder(@NonNull View itemView, ProviderRequestInterface providerRequestInterface) {
+
+        public RequestViewHolder(@NonNull View itemView, ProviderRequestHistoryInterface providerRequestHistoryInterface) {
             super(itemView);
-            userName=itemView.findViewById(R.id.userName);
-            status=itemView.findViewById(R.id.status);
-            breakDownType=itemView.findViewById(R.id.breakdowntype);
-            location=itemView.findViewById(R.id.location);
+            userName = itemView.findViewById(R.id.userName);
+            status = itemView.findViewById(R.id.status);
+            breakDownType = itemView.findViewById(R.id.breakdowntype);
+            location = itemView.findViewById(R.id.location);
             imageViewProfilePic = itemView.findViewById(R.id.imageViewProfilePic);
-            btnCompleted = itemView.findViewById(R.id.btnComplete);
-
-            btnCompleted.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (providerRequestInterface != null) {
-                        int pos = getAdapterPosition();
-
-                        if (pos != RecyclerView.NO_POSITION) {
-                            // Call a method in the interface to handle the click event
-                            providerRequestInterface.onCompleteClick(pos);
-                        }
-                    }
-                }
-            });
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(providerRequestInterface !=null){
-                        int  pos = getAdapterPosition();
+                    if (providerRequestHistoryInterface != null) {
+                        int pos = getAdapterPosition();
 
-                        if(pos != RecyclerView.NO_POSITION){
-                            providerRequestInterface.OnItemClick(pos);
+                        if (pos != RecyclerView.NO_POSITION) {
+                            providerRequestHistoryInterface.OnItemClick(pos);
                         }
                     }
                 }
